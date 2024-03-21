5357fb108b19f7378c1e5b38af04e1b1c64772f5
Zelei Mátyás <zeleimatyi@gmail.com>
2024-03-21 12:55:08 +0100

    Trying Textures

--- Programs/Skeleton/Skeleton/Skeleton.cpp
+++ Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -66,10 +66,14 @@ const char * const vertexSource = R"(
 	precision highp float;		// normal floats, makes no difference on desktop computers
 
 	uniform mat4 MVP;			// uniform variable, the Model-View-Projection transformation matrix
-	layout(location = 0) in vec3 vp;	// Varying input: vp = vertex position is expected in attrib array 0
+	layout(location = 0) in vec2 vp;	// Varying input: vp = vertex position is expected in attrib array 0
+    layout(location = 1) in vec2 vtxUV;
+
+    out vec2 texcoord;
 
 	void main() {
 		gl_Position = vec4(vp.x, vp.y, vp.z, 1) * MVP;		// transform vp from modeling space to normalized device space
+        texcoord = vtxUV;
 	}
 )";
 
@@ -77,11 +81,12 @@ const char * const fragmentSource = R"(
 	#version 330			// Shader 3.3
 	precision highp float;	// normal floats, makes no difference on desktop computers
 
-	uniform vec3 color;		// uniform variable, the color of the primitive
-	out vec4 outColor;		// computed color of the current pixel
+	uniform sampler2D samplerUnit;
+    in vec2 texcoord;
+    out vec4 fragmentColor;
 
 	void main() {
-		outColor = vec4(color, 1);	// computed color is the color of the primitive
+		fragmentColor = texture(samplerUnit, texcoord);
 	}
 )";
 
@@ -249,19 +254,21 @@ public:
 			}
 		}
 
-		glGenVertexArrays(1, &vao);
-
-		glBindVertexArray(vao);
-		glGenBuffers(2, vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), reinterpret_cast<void*>(0));
-
-		glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+        glGenBuffers(2, vbo);// Generate 2 vertex buffer objects
+// vertex coordinates: vbo[0] -> Attrib Array 0 -> vertices
+        glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+        float vtxs[] = {-0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f};
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vtxs),vtxs, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+// vertex coordinates: vbo[1] -> Attrib Array 1 -> uvs
+        glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+        float uvs[] = {0, 0, 1, 0, 1, 1, 0, 1};
+        glBufferData(GL_ARRAY_BUFFER, sizeof(uvs), uvs, GL_STATIC_DRAW);
         glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), reinterpret_cast<void*>(0));
-
-
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, NULL);
 
 
 	}
@@ -270,6 +277,8 @@ public:
 		gpuProgram.setUniform(sampler, "samplerUnit");
 		glActiveTexture(GL_TEXTURE0 + sampler); // = GL_TEXTURE0
 		glBindTexture(GL_TEXTURE_2D, textureId);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 4);
 
 	}
 };
